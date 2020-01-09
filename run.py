7719731cb23e505d04ee4bbcdc9d7372845dd225
mikewojtkiewicz <mikewojtkiewicz@gmail.com>
2020-01-09 14:45:27 -0500

    initial commit

--- run.py
+++ run.py
@@ -8,13 +8,15 @@ def main():
         start_time = datetime.now()
         folder_location = input("Enter Folder Location: ")
         output_location = input("Enter Output Location: ")
+        trim_start = input("Enter Seconds to Trim From Start: ")
+        trim_end = input("Enter Seconds to Trim From End: ")
         folder = os.listdir(folder_location)
         print(f'{start_time}: Processing {len(folder)} files...')
         for file in folder:
             if file.endswith('.mp4'):
                 file_name = f"{folder_location}\\{file}"
-                video_file = VideoFileClip(file_name).cutout(0, 18)
-                new_file_duration = float(video_file.duration) - 6.5
+                video_file = VideoFileClip(file_name).cutout(0, float(trim_start))
+                new_file_duration = float(video_file.duration) - float(trim_end)
                 video_file.duration = new_file_duration
 
                 name_array = file.split('-')
